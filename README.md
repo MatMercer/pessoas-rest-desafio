8e98b830689cfbd75d4e78aeff92d16ecd442c23
MatMercer <contact@matbm.net>
2020-07-16 23:00:56 -0300

    Adiciona source.html e README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# API REST Pessoas Desafio
+
+## Para rodar
+```
+docker run -p 8080:8080/tcp matmercer/pessoas-rest
+```
---
+++ src/main/java/net/matbm/pessoas/presenter/rest/v1/SourceController.java
@@ -0,0 +1,37 @@
+package net.matbm.pessoas.presenter.rest.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequiredArgsConstructor
+public class SourceController {
+
+    @GetMapping(value = "/", produces = "text/html")
+    public String redirect() {
+        return "redirect:source";
+    }
+
+    @GetMapping(value = "/source", produces = "text/html")
+    public ResponseEntity<String> source() {
+        try {
+            var file = ResourceUtils.getFile("classpath:source.html");
+            var stream = new FileInputStream(file);
+            var bytes = stream.readAllBytes();
+
+            return ResponseEntity.ok(new String(bytes, 0, bytes.length, StandardCharsets.UTF_8));
+        } catch (FileNotFoundException e) {
+            return ResponseEntity.ok("source.html não encontrado");
+        } catch (IOException e) {
+            return ResponseEntity.ok("falha ao ler source.html");
+        }
+    }
+}
